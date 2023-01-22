c8390e15ce43d3d119518ca956c624d139cc92cf
Luke Vance <luke.vance@trustly.com>
2023-01-22 13:19:13 -0500

    moved establishData to function for dynamic render

--- react-web-app/src/App.js
+++ react-web-app/src/App.js
@@ -5,9 +5,6 @@ const MERCHANT_ID = 'YOUR_MERCHANT_ID';
 const serverURL = 'YOUR_SERVER_URL';
 
 function App() {
-
-  // const params = new URLSearchParams(window.location.search);
-  
   const TrustlyOptions = {
     closeButton: false,
     dragAndDrop: true,
@@ -15,8 +12,7 @@ function App() {
   };
 
   const returnEstablishData = () => {
-    // TODO: add logic based on params
-    return {
+    let data = {
       accessId: ACCESS_ID,
       // requestSignature: REQUEST_SIGNATURE,
       merchantId: MERCHANT_ID,
@@ -25,11 +21,9 @@ function App() {
       paymentType: 'Retrieval',
       returnUrl: `${serverURL}/return`,
       cancelUrl: `${serverURL}/cancel`,
-      metadata: {
-        urlScheme: "in-app-browser-ios://",
-        integrationContext: "InAppBrowser",
-      }  
-    }
+      metadata: {}  
+    };
+    return data;
   };
 
   return (
