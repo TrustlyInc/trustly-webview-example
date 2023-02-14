780c709dcc4b416aaca361b007639528f3dfa3c3
Andre Guedes <andre.guedes@trustly.com>
2023-02-14 16:22:15 -0300

    Show custom tabs for oauth login

--- android-app/.idea/sonarlint/issuestore/index.pb
+++ android-app/.idea/sonarlint/issuestore/index.pb
@@ -11,5 +11,3 @@ t
 Dapp/src/test/java/net/trustly/inappbrowserandroid/ExampleUnitTest.kt,d/7/d7e4b227def4f1aa2aa7e3b953ce40cbb75054fe
 ?
 settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
-@
-app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
--- android-app/app/src/main/java/net/trustly/inappbrowserandroid/MainActivity.kt
+++ android-app/app/src/main/java/net/trustly/inappbrowserandroid/MainActivity.kt
@@ -1,10 +1,15 @@
 package net.trustly.inappbrowserandroid
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +28,28 @@ class MainActivity : AppCompatActivity() {
             domStorageEnabled = true
         }
 
-        webView.webViewClient = WebViewClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                try {
+                    val url = request.url.toString()
+                    if (url.contains("/oauth/login"))
+                        launchUrl(this@MainActivity, url)
+                    return false
+                } catch (e: Exception) {
+                    Log.e("MainActivity", e.message.toString())
+                }
+                return true
+            }
+        }
         webView.loadUrl("http://localhost:3000?integrationContext=InAppBrowser&urlScheme=in-app-browser-android")
+    }
+
+    private fun launchUrl(context: Context, url: String) {
+        val customTabsIntent = CustomTabsIntent.Builder().build()
+        customTabsIntent.launchUrl(context, Uri.parse(url))
     }
 
 }
